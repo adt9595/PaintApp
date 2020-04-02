51f0ad3364399d0f58134a46347b8e47d9af4f91
adt9595 <58305256+adt9595@users.noreply.github.com>
2020-04-02 11:49:36 +0100

    UI cleaned up + bug fixes
    Changed the toolbar colour scheme
    
    Removed space between colour selectors
    
    Moved and changed the appearance of the clear button to better fit the UI
    
    Removed the canvas border
    
    + other minor fixes

--- painter.js
+++ painter.js
@@ -1,6 +1,6 @@
 var colourDict={
-	0: '#000000', // Black
-	1: '#ffffff', // White
+	0: '#ffffff', // White
+	1: '#000000', // Black
 	2: '#b84248', // Light Red
 	3: '#73272b', // Dark Red
 	4: '#5a9acc', // Light Blue
@@ -16,21 +16,28 @@ var brushDict={
 }
 
 window.addEventListener('load',() =>{
+	// Get canvases
 	var canvases = [document.querySelector('#layer1'),
 					document.querySelector('#layer2'),
 					document.querySelector('#layer3')];
 	const brushCanvas = document.querySelector('#brushCanvas');
+
+	// Get contexts
 	var contexts = [canvases[0].getContext('2d'),
 					canvases[1].getContext('2d'),
 					canvases[2].getContext('2d')]
 	const brushContext = brushCanvas.getContext('2d');
 	var currentContext = contexts[0];
+
 	const inputColour = document.getElementsByClassName('inputColour')[0];
+	inputColour.addEventListener('input',colourUpdate);
+
 	const colourButtons = document.querySelectorAll('div.colour');
 	const layerButtons = document.querySelectorAll('div.layerSelector');
 	const brushButtons = document.querySelectorAll('div.brushStyleSelector');
+	const clearButton = document.getElementById('clearButton');
+	clearButton.addEventListener('click',clearCanvas);
 	
-
 	let brush = document.getElementById('brush');
 	
 	numColours = colourButtons.length;
@@ -40,12 +47,11 @@ window.addEventListener('load',() =>{
 	let brushSize = currentContext.lineWidth;
 	let isDrawing = false;
 	let isErasing = false;
-	let isFilling = false;
 	let brushColour;
 	brushColour = '#000000';
 	let currentBrushStyle = 'round';
 
-	// Add colour button links
+	// Add colour button listeners
 	for(i=0;i<numColours;i++){
 		(function(){
 			var self = colourButtons[i];
@@ -57,7 +63,7 @@ window.addEventListener('load',() =>{
 		}());
 	}
 
-	// Add layer button links
+	// Add layer button listeners
 	for(i=0;i<numLayers;i++){
 		(function(){
 			var index = i;
@@ -67,7 +73,7 @@ window.addEventListener('load',() =>{
 		}());
 	}
 
-	// Add brush button links
+	// Add brush style button listeners
 	for(i=0;i<numLayers;i++){
 		(function(){
 			var index = i;
@@ -88,18 +94,17 @@ window.addEventListener('load',() =>{
 	brushCanvas.width = canvases[0].width;
 
 	function colourUpdate(){
+		brushColour = inputColour.value;
 		brushContext.fillStyle = brushColour;
 		let rectDimensions;
+		let colourOffset = document.getElementsByClassName('inputColour')[0].offsetLeft;//inputColour.offsetLeft;
 		if(window.innerWidth > 1270){
 			rectDimensions = 0.04*window.innerWidth;
 		}
 		else{
 			rectDimensions = 53;
 		}
-		// brushContext.globalAlpha = 0.2;
-		//brushContext.fillRect(0.0495*window.innerWidth,0.085*window.innerHeight,rectDimensions,rectDimensions);
-		brushContext.fillRect(58.6,0.085*window.innerHeight,rectDimensions,rectDimensions);
-		// brushContext.globalAlpha = 1.0;
+		brushContext.fillRect(colourOffset,0.085*window.innerHeight,rectDimensions,rectDimensions);
 	}
 
 	function beginDraw(){
@@ -135,9 +140,6 @@ window.addEventListener('load',() =>{
 
 	function drawBrush(e){
 		brushContext.clearRect(0, 0, brushCanvas.width, brushCanvas.height);
-		if(isFilling){	
-			return;
-		}
 		colourUpdate();
 		computeDraw(e,brushContext,brushColour);
 	}
@@ -177,22 +179,6 @@ window.addEventListener('load',() =>{
 		currentContext.clearRect(0, 0, brushCanvas.width, brushCanvas.height);
 	}
 
-	// function imageToArray(){
-	// 	var rows = canvas.height;
-	// 	var cols = canvas.width;
-	// 	var array = new Array(rows);
-	// 	for(var i=0;i<array.length;i++){
-	// 		array[i] = new Array(cols);
-	// 	}
-	// 	for(var i=0;i<rows;i++){
-	// 		for(var j=0;j<cols;j++){
-	// 			array[i][j] = 255;
-	// 		}
-	// 	}
-	// 	return array;
-	// }
-
-
 	function rgbToHex(r,g,b){
 		return "#" + colToHex(r) + colToHex(g) + colToHex(b);
 	}
@@ -211,16 +197,8 @@ window.addEventListener('load',() =>{
 	    var r = (bigint >> 16) & 255;
 	    var g = (bigint >> 8) & 255;
 	    var b = bigint & 255;
-
 	    return [r,g,b];
 	}
-
-	// function undefinedToWhite(pixel){
-	// 	if(pixel === undefined){
-	// 		return 0;
-	// 	}
-	// 	return pixel;
-	// }
 
 	function changeContext(index){
 		currentContext = contexts[index]
@@ -231,61 +209,19 @@ window.addEventListener('load',() =>{
 		currentBrushStyle = brushDict[index];
 	}
 
-	// function fillInit(e){
-	// 	var image = context.getImageData(0,0,canvas.width,canvas.height).data;
-	// 	var imgArray = imageToArray();
-	// 	var position = getMousePos(e);
-	// 	console.log(position);
-	// 	var targetColour = brushColour;
-	// 	console.log(typeof position);
-	// 	console.log(image.length);
-	// 	console.log(4*canvas.width*canvas.height);
-	// 	for(var i=0; i<canvas.height;i++){
-	// 		for(var j=0; j<canvas.width;j++){
-	// 			imgArray[i][j] = undefinedToWhite(image[0][canvas.width*i + j]);
-	// 			imgArray[i][j+1] = undefinedToWhite(image[1][canvas.width*i + j]);
-	// 			imgArray[i][j+2] = undefinedToWhite(image[2][canvas.width*i + j]);
-	// 		}	
-	// 	}
-	// 	// try{
-	// 	initialColour = rgbToHex(imgArray[position[1]][3*position[0]],imgArray[position[1]][3*position[0]+1],imgArray[position[1]][3*position[0]+2]);
-	// 	// }
-	//  //    catch(error){
-	//  //    	console.log(imgArray.length);
-	//  //    	console.log(imgArray[0].length);
-	//  //    	console.log(position[1]);
-	//  //    	console.log(3*position[0]);
-	//  //    }
-		
-	// 	floodFill(position,initialColour,targetColour,imgArray);
-	// }
+	function getMousePos(e) {
+        var rect = brushCanvas.getBoundingClientRect();
+        return [(e.clientX - rect.left)/(rect.right-rect.left)*brushCanvas.width,
+        		(e.clientY - rect.top)/(rect.bottom-rect.top)*brushCanvas.height];
+  	}
 
-	// function floodFill(position,initialColour,targetColour,imgArray){
-	// 	if(initialColour === targetColour){
-	// 		return;
-	// 	}
-	// 	context.fillStyle = targetColour;
-	// 	context.fillRect(position[0],position[1],1,1);
-	// 	hexTargetColour = hexToRgb(targetColour);
-	// 	imgArray[position[1]][3*position[0]+1] = hexTargetColour[0];
-	// 	imgArray[position[1]][3*position[0]+2] = hexTargetColour[1];
-	// 	imgArray[position[1]][3*position[0]+3] = hexTargetColour[2];
-	// 	// imgArray[position[0],position[1]] = hexToRgb(targetColour)[0];
-	// 	// imgArray[position[0],position[1]+1] = hexToRgb(targetColour)[1];
-	// 	// imgArray[position[0],position[1]+2] = hexToRgb(targetColour)[2];
-	// 	floodFill([position[0]+1,position[1]],initialColour,targetColour,imgArray);
-	// 	floodFill([position[0]-1,position[1]],initialColour,targetColour,imgArray);
-	// 	floodFill([position[0],position[1]-1],initialColour,targetColour,imgArray);
-	// 	floodFill([position[0],position[1]+1],initialColour,targetColour,imgArray);
-		
-	// 	// floodFill([position[0]+1,position[1]],rgbToHex(imgArray[position[1]][3*(position[0]+1)],imgArray[position[1]][3*(position[0]+1)+1],imgArray[position[1]][3*(position[0]+1)+2]),targetColour,imgArray);
-	// 	// floodFill([position[0]-1,position[1]],rgbToHex(imgArray[position[1]][3*(position[0]-1)],imgArray[position[1]][3*(position[0]-1)+1],imgArray[position[1]][3*(position[0]-1)+2]),targetColour,imgArray);
-	// 	// floodFill([position[0],position[1]-1],rgbToHex(imgArray[position[1]-1][3*position[0]],imgArray[position[1]-1][3*position[0]+1],imgArray[position[1]-1][3*position[0]+2]),targetColour,imgArray);
-	// 	// floodFill([position[0],position[1]+1],rgbToHex(imgArray[position[1]+1][3*position[0]],imgArray[position[1]+1][3*position[0]+1],imgArray[position[1]+1][3*position[0]+2]),targetColour,imgArray);
-	// 	return;
-	// }	
+  	// Disable context menu on right-click
+  	document.oncontextmenu = function() {
+	    return false;
+	}
 
-	window.addEventListener('resize',() =>{
+	// Window event listeners
+  	window.addEventListener('resize',() =>{
 		for(i=0;i<numLayers;i++){
 			canvases[i].height = window.innerHeight;
 			canvases[i].width = window.innerWidth;
@@ -295,23 +231,11 @@ window.addEventListener('load',() =>{
 		brushCanvas.height = canvases[0].height;
 		brushCanvas.width = canvases[0].width;
 		colourUpdate();
-
 	})
 
-	function getMousePos(e) {
-        var rect = brushCanvas.getBoundingClientRect();
-        return [(e.clientX - rect.left)/(rect.right-rect.left)*brushCanvas.width,
-        		(e.clientY - rect.top)/(rect.bottom-rect.top)*brushCanvas.height];
-  	}
-
-
 	window.addEventListener('mousemove',draw);
+
 	window.addEventListener('mousedown',function(e){
-		// if(isFilling){
-		// 	fillInit(e);
-		// 	isFilling = false;
-		// 	return;
-		// }
 		switch(e.button){
 			case 0:
 				beginDraw();
@@ -322,6 +246,7 @@ window.addEventListener('load',() =>{
 		}
 
 	})
+
 	window.addEventListener('mouseup',function(e){
 		switch(e.button){
 			case 0:
@@ -332,15 +257,9 @@ window.addEventListener('load',() =>{
 				break;
 		}
 	});
+
 	window.addEventListener('wheel',changeSize);
-	document.getElementsByClassName('inputColour')[0].addEventListener('change',colourUpdate);
-	document.getElementById('clearButton').addEventListener('click',clearCanvas);
-	// document.getElementById('bucket').addEventListener('click',function(e){
-	// 	isFilling = true;
-	// });
-	document.oncontextmenu = function() {
-	  return false;
-	}
+	
 	colourUpdate();
 })
 
